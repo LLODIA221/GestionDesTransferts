c8bc57e3948ccf8461e5faa44fb1e795c0be0c59
LLODIA221 <cheikhdkgdiallo@gmail.com>
2024-09-29 10:08:48 +0000

    dimanche test

--- src/main/java/com/Application/GestionDesTransferts/Models/Association.java
+++ src/main/java/com/Application/GestionDesTransferts/Models/Association.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Entity
 @Data
 public class Association {
+    // ceci est un commentaire
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAssociation;
